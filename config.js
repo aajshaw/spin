8ab3b668f01659283df9ca7a45cffcc3efa63e07
Anthony Shaw <tony@adshaw.uk>
2018-03-12 15:01:44 +0000

    Added more sound effects

--- config.js
+++ config.js
@@ -12,9 +12,9 @@ nconf.defaults({
   "windowShowMenu": false,
   "windowIcon": __dirname + "/assets/spin.ico",
   "soundDailyKw" : __dirname + "/assets/Blastwave_FX_CashRegister_S08IN_92.wav",
-  "soundLifetime10Kw" : __dirname + "",
-  "soundLifetime100Kw": __dirname + "",
-  "soundLifetime1Mw": __dirname + ""
+  "soundLifetime10Kw" : __dirname + "/assets/zapsplat_sound_design_ascending_metalic_tone_powerful_electric.wav",
+  "soundLifetime100Kw": __dirname + "/assets/zapsplat_emergency_alarm_siren.wav",
+  "soundLifetime1Mw": __dirname + "/assets/Blastwave_FX_ExplosionCrash_S08WA_132.wav"
 });
 
 module.exports = {
